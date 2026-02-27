571478b660470ec573ea3c3c974711f7e8dd65d5
Saumitra Bagchi <saumitra.ku.cse@gmail.com>
2026-02-27 09:50:58 +0200

    Initial project structure with Spring Boot backend and React frontend

---
+++ restaurant_reservation_api/src/main/java/com/example/saumitra/restaurant_reservation_api/RestaurantReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.saumitra.restaurant_reservation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationApiApplication.class, args);
+	}
+
+}
---
+++ restaurant_reservation_api/src/test/java/com/example/saumitra/restaurant_reservation_api/RestaurantReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saumitra.restaurant_reservation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
